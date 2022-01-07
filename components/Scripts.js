8790ccb4d2ce3f85f349ac989f78ad5c7afbf42e
Louis-yes <luegraham@gmail.com>
2022-01-07 21:46:53 +1100

    Moved everything from return.horse to here

---
+++ components/Scripts.js
@@ -0,0 +1,5 @@
+export default function Scripts(site){
+    return `
+        <script src="${site.url}/js/scripts.js"></script>
+    `
+}
---
+++ components/Styles.js
@@ -0,0 +1,9 @@
+export default function styles(site){
+    return `
+        <link rel="stylesheet" href="${site.url}/css/normalize.css">
+        <link rel="stylesheet" href="${site.url}/css/style.css">
+        <style>
+
+        </style>
+    `
+}
