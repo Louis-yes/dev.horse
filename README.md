901869c0cf008bdd0cc6baf774bdccb393c54027
Louis-yes <luegraham@gmail.com>
2022-12-15 18:10:05 +1100

    Refreshed, minimal sun bleached bones in the desert

---
+++ README.md
@@ -0,0 +1,8 @@
+# Everyday a cowboy rides out in the morning and returns in the evening.
+![](image/../images/full.png
+
+This repo contains the pieces I use to create [return.horse](https://www.return.horse). They're pulled together by a third component, currently living on my hard-drive, called forge.horse.
+
+- drafts: For backfilling, or for ideas I haven't settled on yet.
+- posts: the meat of the meal
+- images/: the shadows on the cave wall
---
+++ drafts.js
@@ -0,0 +1,317 @@
+[
+ {
+  "title": "jlk",
+  "date": "2",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "20",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "202",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "2022",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "title",
+  "date": "dated",
+  "path": "path",
+  "description": "description",
+  "hovertext": "hovertext",
+  "panels": [
+   {
+    "text": "panel 1",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "panel 2",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 825
+   }
+  ]
+ },
+ {
+  "title": "title",
+  "date": "date",
+  "path": "path",
+  "description": "description",
+  "hovertext": "hovertext",
+  "panels": [
+   {
+    "text": "panel 1",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "panel 2",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 825
+   }
+  ]
+ },
+ {
+  "title": "",
+  "date": "",
+  "path": "",
+  "description": "",
+  "hovertext": "",
+  "panels": [
+   {
+    "text": "hold on\n",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 810
+   },
+   {
+    "text": "",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 825
+   }
+  ]
+ },
+ {
+  "title": "title",
+  "date": "daten",
+  "path": "path",
+  "description": "description",
+  "hovertext": "hovertext",
+  "panels": [
+   {
+    "text": "panel 1",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "panel 2",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 825
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "2022-",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "2022-1",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "2022-10",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "2022-10-",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "2022-10-2",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ },
+ {
+  "title": "jlk",
+  "date": "2022-10-29",
+  "path": "ll",
+  "description": "ll\nl",
+  "hovertext": "kk",
+  "panels": [
+   {
+    "text": "hold on",
+    "alt": "A cowboy riding left to right, smoking. The sun is up and there are birds in the sky",
+    "x": 302,
+    "y": 825
+   },
+   {
+    "text": "what's that \ngalloping sound",
+    "alt": "A cowboy riding right to left, smoking. The moon is up and there are stars in the sky",
+    "x": 782,
+    "y": 810
+   }
+  ]
+ }
+]
