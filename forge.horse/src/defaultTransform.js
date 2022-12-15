d3c5dd909d58680b933e9a58d3d04247a611fd29
Louis-yes <luegraham@gmail.com>
2022-12-15 18:04:30 +1100

    Clearing out old files for a new and simpler world

--- forge.horse/src/defaultTransform.js
+++
@@ -1,4 +0,0 @@
-const defaultTransform = [
-    
-]
-export default defaultTransform
--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-const rr = document.querySelector("#random")
-rr.addEventListener("mouseover", (e)=>{
-  if(rr && window.comics && window.site){
-    let rn = Math.floor(window.comics.length * Math.random())
-    rr.href = `${window.site.url}/${window.comics[rn].path}`
-  }
-})
